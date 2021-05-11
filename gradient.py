51bbb05ff01328d3ec2de1afa249f4606b2b95b3
JEON <ohgnu90@korea.ac.kr>
2021-05-11 20:07:44 +0900

    visualization only mode

--- gradient.py
+++ gradient.py
@@ -111,25 +111,53 @@ def main(path, opath, is_ms):
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser(description='This script calculates the gradients with data deduplication.')
-    parser.add_argument('--fpath', type=str, required=True, help='directory including data files. if path includes white spaces or Korean, wrap the entire path with double quotes.')
+    parser.add_argument('--fpath', type=str, required=False, help='directory including data files. if path includes white spaces or Korean, wrap the entire path with double quotes.')
     parser.add_argument('--opath', type=str, required=False, help='path to save the output. if you don\'t specify it, the current directory is inplaced.', default=os.getcwd())
     parser.add_argument('-MS', help='include this command in your arguments', action='store_true')
+    parser.add_argument('-V', help='visual mode for the results in opath. Data analyzing does not performed.', action='store_true')
 
     args = parser.parse_args()
 
-    fpath, opath, is_ms = args.fpath, args.opath, args.MS
+    fpath, opath, is_ms, is_visual = args.fpath, args.opath, args.MS, args.V
+    if not is_visual:
+        if fpath is None or fpath == '':
+            print('[!] you must specify the fpath in analyzing mode.')
+            exit(1)
+
     print('[*] path to file:', fpath)
     print('[*] path to save:', opath)
     print('[*] with ms:', is_ms)
+    print('[*] in visual mode:', is_visual)
     print('[*] the title of plot may not be properly expressed if you use the file name in Korean.')
 
-    fpath, opath = sanitize_source_path(fpath, opath)
+    if not is_visual:
+        fpath, opath = sanitize_source_path(fpath, opath)
 
-    source_files = [os.path.join(fpath, path) for path in os.listdir(fpath) if not os.path.isdir(os.path.join(fpath, path))]
-    source_files.sort()
+        source_files = [os.path.join(fpath, path) for path in os.listdir(fpath) if not os.path.isdir(os.path.join(fpath, path))]
+        source_files.sort()
 
-    for spath in source_files:
-        print('[*] start to analyze', spath)
-        main(spath, opath, is_ms)
+        for spath in source_files:
+            print('[*] start to analyze', spath)
+            main(spath, opath, is_ms)
+    else:
+        data_files = [os.path.join(opath, path) for path in os.listdir(opath) if not os.path.isdir(os.path.join(opath, path)) and 'xlsx' in path.lower()]
+        if not data_files:
+            print('opath must contain at least one data file(.xlsx).')
+            exit(1)
+
+        data_files.sort()
+
+        for dpath in data_files:
+            print('[*] plot', dpath)
+            try:
+                fname = pathlib.Path(dpath).name
+                data = pd.read_excel(dpath, header=0)
+                hours = data['hour'].to_numpy()
+                values = data['Hn'].to_numpy()
+                gradients = data['gradient'].to_numpy()
+                do_plot(hours, values, fname)
+
+            except PermissionError:
+                pass
 
     plt.show()
